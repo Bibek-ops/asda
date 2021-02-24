2bfa1d5964c9c45ac23b29deb57ff4024a19a2a0
TOBI <bibekshr777@gmail.com>
2021-02-24 11:27:22 +0545

    area of triangle

---
+++ lab1/area.py
@@ -0,0 +1,6 @@
+#WAP that reads the length of the base adn the height of a right angled triangle and print the area.
+a=int(input("enter height of the triangle :"))
+b=int(input("enter breadth of the triangle:"))
+A=a*b/2
+print(A)
+
