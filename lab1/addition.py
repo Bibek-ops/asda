aa741c64eead9492c4dd531311e3f24e45fe2b99
TOBI <bibekshr777@gmail.com>
2021-02-24 11:18:25 +0545

    Initial commit

---
+++ lab1/addition.py
@@ -0,0 +1,7 @@
+#WAP that takes three numbers and prints their sum. Every number is given on a seperate line.
+a=int(input("enter first number :"))
+b=int(input("enter second number :"))
+c=int(input("enter third number :"))
+s=a+b+c
+print(s)
+
